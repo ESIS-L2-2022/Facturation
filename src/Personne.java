632f79cc21299e67deaa57013ae7916e6a1978c3
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-06-23 20:57:41 +0200

    adresse as an inner class

--- src/Personne.java
+++ src/Personne.java
@@ -41,4 +41,84 @@ public abstract class Personne {
     public void setAdresse(Adresse adresse) {
         this.adresse = adresse;
     }
+
+
+    public class Adresse {
+        private String numero;
+        private String avenue;
+        private String cellule;
+        private String quartier;
+        private String commune;
+        private String ville;
+        private String pays;
+
+        public Adresse(String numero, String avenue, String cellule, String quartier, String commune, String ville, String pays) {
+            this.numero = numero;
+            this.avenue = avenue;
+            this.cellule = cellule;
+            this.quartier = quartier;
+            this.commune = commune;
+            this.ville = ville;
+            this.pays = pays;
+        }
+
+        public Adresse() {
+        }
+
+        public String getNumero() {
+            return numero;
+        }
+
+        public void setNumero(String numero) {
+            this.numero = numero;
+        }
+
+        public String getAvenue() {
+            return avenue;
+        }
+
+        public void setAvenue(String avenue) {
+            this.avenue = avenue;
+        }
+
+        public String getCellule() {
+            return cellule;
+        }
+
+        public void setCellule(String cellule) {
+            this.cellule = cellule;
+        }
+
+        public String getQuartier() {
+            return quartier;
+        }
+
+        public void setQuartier(String quartier) {
+            this.quartier = quartier;
+        }
+
+        public String getCommune() {
+            return commune;
+        }
+
+        public void setCommune(String commune) {
+            this.commune = commune;
+        }
+
+        public String getVille() {
+            return ville;
+        }
+
+        public void setVille(String ville) {
+            this.ville = ville;
+        }
+
+        public String getPays() {
+            return pays;
+        }
+
+        public void setPays(String pays) {
+            this.pays = pays;
+        }
+    }
 }
